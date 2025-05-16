8a95393f8e0d14eee429a1c37ff4351b0f222e49
Marc Igneri <marc@munchmode.com>
2025-05-16 11:28:37 -0400

    v1.0

--- middlewares/checkRole.js
+++
@@ -1,8 +0,0 @@
-const checkRole = (requiredRole) => (req, res, next) => {
-    if (req.user.role !== requiredRole) {
-        return res.status(403).json({ error: 'Forbidden: Insufficient permissions' });
-    }
-    next();
-};
-
-module.exports = checkRole;
--- utils/validators.js
+++
@@ -1,9 +0,0 @@
-const Joi = require('joi');
-
-const cohortSchema = Joi.object({
-    topic: Joi.string().required(),
-    phaseLockingValue: Joi.number().min(0).max(1).required(),
-    groupBandpower: Joi.number().min(0).optional(),
-});
-
-module.exports = { cohortSchema };
